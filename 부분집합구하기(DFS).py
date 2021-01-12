e27946fd7ba0ee638d8d017b38ecb4c93447bcc1
Jeon Ji Yoon <rebegin15@naver.com>
2021-01-12 14:47:05 +0900

    0112

---
+++ 부분집합구하기(DFS).py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+def DFS(x):
+    if(x==n+1):
+        #print
+        for i in range (1, n+1):
+            if(ch[i] == 1):
+                print(i,end=" ")
+            else:
+                continue
+        print("\n")
+
+
+    else:
+        ch[x] = 1
+        DFS(x+1)
+        ch[x] = 0
+        DFS(x+1)
+        
+
+if __name__ == "__main__":
+    n = int(input())
+    ch=[0]*(n+1)
+    DFS(1)
+    
