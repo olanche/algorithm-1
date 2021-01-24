80e83c41442e8f603a464d35894c7fde2cd13071
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-24 22:19:04 +0900

    0124 수열 추측하기 첫번째시도_해결방법 : DFS로 모든 경우의 수를 전부 탐색한다

---
+++ DFS - 수열추측하기(순열, 파스칼 응용).py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def DFS(L):
+    if(L == N):
+        for i in range(0,N):
+            list1[i] = 요소
+        if(sizeof(list) = N):
+            print()
+            프로그램 종료
+    else:
+        for i in range(1,N+1):
+            if( i in isList):
+                continue
+            else:
+                list.append()
+
+
+if __name__ == "__main__":
+    N, F=map(int, input().split())
+    DFS(0)
