2c6914ad6a4f423f8d78f661ebc2ba37d3005ee0
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-24 22:24:40 +0900

    0124 수열추측하기 첫시도_나올 수 있는 모든 수열을 DFS로 탐색한다

--- DFS - 수열추측하기(순열, 파스칼 응용).py
+++ DFS - 수열추측하기(순열, 파스칼 응용).py
@@ -4,18 +4,21 @@ sys.stdin = open("input.txt", "r")
 def DFS(L):
     if(L == N):
         for i in range(0,N):
-            list1[i] = 요소
+            resTest[i] = #요소
         if(sizeof(list) = N):
             print()
-            프로그램 종료
+            #프로그램 자체를 종료하기
     else:
         for i in range(1,N+1):
             if( i in isList):
                 continue
             else:
-                list.append()
+                res.append()
 
 
 if __name__ == "__main__":
     N, F=map(int, input().split())
     DFS(0)
+    isList = [0]*(N+1)
+    res = [0]*N
+    resTest = [0]*N
