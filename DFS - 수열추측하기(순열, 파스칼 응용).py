ca574382aff19c9f524a6487557e9fee7ba718d7
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-25 23:41:37 +0900

    0125 중복을 허락하지 않는 순열을 구하는 것과 비슷한 문제이다

--- DFS - 수열추측하기(순열, 파스칼 응용).py
+++ DFS - 수열추측하기(순열, 파스칼 응용).py
@@ -3,22 +3,25 @@ sys.stdin = open("input.txt", "r")
 
 def DFS(L):
     if(L == N):
-        for i in range(0,N):
-            resTest[i] = #요소
-        if(sizeof(list) = N):
-            print()
-            #프로그램 자체를 종료하기
+        for i in range(N-1, 0, -1):
+            for j in range(0,i):
+                res[j] = res[j]+res[j+1]
+        if(res[0] == F):
+            print(res[0])
+            #프로그램 자체를 종료한다
     else:
         for i in range(1,N+1):
-            if( i in isList):
+            if(ch[i] == 1):
                 continue
-            else:
-                res.append()
-
-
+            else:                
+                ch[i] = 1
+                res.append(i)
+                DFS(L+1)  
+                ch[i] = 0
+                res.pop()
+                
 if __name__ == "__main__":
     N, F=map(int, input().split())
+    ch = [0]*(N+1)
+    res = []
     DFS(0)
-    isList = [0]*(N+1)
-    res = [0]*N
-    resTest = [0]*N
