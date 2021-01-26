2a8cdc935dd8a0d9d064ee29126bcab98c596016
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-26 21:42:47 +0900

    0126 완성

--- DFS - 수열추측하기(순열, 파스칼 응용).py
+++ DFS - 수열추측하기(순열, 파스칼 응용).py
@@ -1,30 +1,28 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def DFS(L):
-    if(L == N):
-        resTemp = res
-        for i in range(N-1, 0, -1):
-            for j in range(0,i):
-                res[j] = res[j]+res[j+1]
-
-        if(res[0] == F):
-            print(resTemp)
-            #프로그램 자체를 종료한다
+def DFS(L, sum):
+    if(L == N and sum == F):
+        #N-1의 이항계수들을 구한다... 예를 들어 N=4 이면 1 3 3 1
+        for x in res:
+            print(x, end = " ")
+        sys.exit(0)
     else:
         for i in range(1,N+1):
-            if(ch[i] == 1):
-                continue
-            else:                
+            if(ch[i] == 0):             
                 ch[i] = 1
-                res.append(i)
-                DFS(L+1)  
+                res[L] = i
+                DFS(L+1, sum + (bi[L]*res[L]))  
                 ch[i] = 0
-                res.pop()
                 
 if __name__ == "__main__":
     N, F=map(int, input().split())
     ch = [0]*(N+1)
-    res = []
-    resTemp=[]
-    DFS(0)
+    res = [0]*N
+    bi = [1]*(N)
+    for i in range(1,N):
+        temp = bi[i-1] * (N-i)
+        temp = temp // i
+        bi[i] = temp
+    DFS(0, 0)
+
