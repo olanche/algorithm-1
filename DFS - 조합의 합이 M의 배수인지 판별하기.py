699547efab5f91a090466eded27eb5382feebfdf
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-28 13:59:56 +0900

    0128-조합의 요소들이 필요한게 아니고 sum이 필요하므로 DFS()의 매개변수에 (sum을 나타내는)su를 추가한다, 조합을 구할때는 s(starting point)가 필요하다

--- DFS - 조합의 합이 M의 배수인지 판별하기.py
+++ DFS - 조합의 합이 M의 배수인지 판별하기.py
@@ -1,24 +1,21 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def DFS(L, s):
+def DFS(L, s, su):
     global cnt
-    if L == 3:
-        #sum(res)
-        if sum(res) % 6 == 0 :
+    if L == K:
+        if su % 6 == 0 :
             cnt +=1
     else:
         for i in range( s, N    ):
-            #a[i]
-            res[L] = a[i]
-            DFS(L+1, i+1)
+            DFS(L+1, i+1, su + a[i])
 
 
 if __name__ == "__main__":
     N, K=map(int, input().split())
     a = list(map(int, input().split()))
     M = input()
-    res = [0]*K
+    #res = [0]*K
     cnt = 0
-    DFS(0,0)
+    DFS(0,0,0)
     print(cnt)
